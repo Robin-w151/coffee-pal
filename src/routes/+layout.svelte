0f1b1826ef73626f54d6665e76e11968e9b4349e
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-07-21 20:02:18 +0200

    add github link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 
   import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton';
   import { faMugHot } from '@fortawesome/free-solid-svg-icons';
+  import { faGithub } from '@fortawesome/free-brands-svg-icons';
   import { Icon } from 'svelte-awesome';
 </script>
 
@@ -12,12 +13,20 @@
   <svelte:fragment slot="header">
     <AppBar padding="px-8 py-4">
       <svelte:fragment slot="lead">
-        <Icon data={faMugHot} scale={1.3} />
+        <Icon data={faMugHot} scale={1.5} />
       </svelte:fragment>
       <span class="text-2xl">Coffee Pal</span>
-      <svelte:fragment slot="trail">
+      <div class="flex items-center gap-4" slot="trail">
+        <a
+          class="btn btn-icon variant-soft hover:variant-soft-primary"
+          href="https://github.com/Robin-w151/coffee-pal"
+          target="_blank"
+          rel="noopener"
+        >
+          <Icon data={faGithub} scale={1.5} />
+        </a>
         <LightSwitch />
-      </svelte:fragment>
+      </div>
     </AppBar>
   </svelte:fragment>
   <div class="p-4">
