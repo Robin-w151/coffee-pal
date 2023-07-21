d575315ae2335b45270fffb3ea80e60dd36c012b
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-07-21 13:23:54 +0200

    add page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
   import Calculator from '$lib/components/calculator/Calculator.svelte';
 </script>
 
+<svelte:head>
+  <title>Coffee Pal</title>
+</svelte:head>
+
 <div class="space-y-4">
   <h2 class="h2">Calculator</h2>
   <div class="card p-4 max-w-screen-md">
