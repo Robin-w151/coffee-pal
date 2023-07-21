59067934a8600752f36c000b3bfa4497f8266392
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-07-21 21:14:17 +0200

    adjust calculator title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 </svelte:head>
 
 <div class="flex flex-col gap-4 items-center">
-  <h2 class="h2">Calculator</h2>
+  <h2 class="h2">Brewing Calculator</h2>
   <div class="card p-4 max-w-screen-md">
     <Calculator />
   </div>
