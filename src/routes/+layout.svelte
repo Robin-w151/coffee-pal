636ac0e5decd374ec30538ca77a3d3f54add9a7e
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-07-21 19:32:19 +0200

    add fontawesome icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,15 @@
   import '../app.scss';
 
   import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton';
-  import CoffeeBeanIcon from '$lib/components/ui/icons/CoffeeBeanIcon.svelte';
+  import { faMugHot } from '@fortawesome/free-solid-svg-icons';
+  import { Icon } from 'svelte-awesome';
 </script>
 
 <AppShell>
   <svelte:fragment slot="header">
-    <AppBar>
+    <AppBar padding="px-8 py-4">
       <svelte:fragment slot="lead">
-        <CoffeeBeanIcon />
+        <Icon data={faMugHot} scale={1.3} />
       </svelte:fragment>
       <span class="text-2xl">Coffee Pal</span>
       <svelte:fragment slot="trail">
