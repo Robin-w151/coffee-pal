c7dac5e48990ece26fca2ef94c8240d454b44e9f
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-07-28 09:48:25 +0200

    add styling for active route in app menu

--- src/lib/components/ui/app/CPAppMenu.svelte
+++ src/lib/components/ui/app/CPAppMenu.svelte
@@ -1,30 +1,48 @@
 <script lang="ts">
-  import { faCalculator, faClipboardList } from '@fortawesome/free-solid-svg-icons';
+  import { page } from '$app/stores';
+  import { faCalculator, faClipboardList, faClose } from '@fortawesome/free-solid-svg-icons';
   import { drawerStore } from '@skeletonlabs/skeleton';
   import { Icon } from 'svelte-awesome';
+
+  const routes = [
+    {
+      href: '/',
+      label: 'Calculator',
+      icon: faCalculator,
+    },
+    {
+      href: '/journal',
+      label: 'Journal',
+      icon: faClipboardList,
+    },
+  ];
 
   function handleClick(): void {
     drawerStore.close();
   }
+
+  $: routeActiveClass = (href: string) =>
+    $page.url.pathname === href ? 'bg-primary-active-token' : '';
 </script>
 
 <section class="flex flex-col gap-4 p-4">
-  <h2 class="h2">Coffee Pal</h2>
+  <div class="flex justify-between items-center">
+    <h2 class="h2">Coffee Pal</h2>
+    <button class="btn btn-icon hover:variant-soft-primary" on:click={handleClick}>
+      <Icon data={faClose} />
+    </button>
+  </div>
   <hr />
   <nav class="list-nav">
     <ul>
-      <li>
-        <a href="/" on:click={handleClick}>
-          <Icon data={faCalculator} />
-          <span>Calculator</span>
-        </a>
-      </li>
-      <li>
-        <a href="/journal" on:click={handleClick}>
-          <Icon data={faClipboardList} />
-          <span>Journal</span>
-        </a>
-      </li>
+      {#each routes as { href, label, icon } (href)}
+        <li>
+          <a {href} class={routeActiveClass(href)} on:click={handleClick}>
+            <Icon data={icon} />
+            <span>{label}</span>
+          </a>
+        </li>
+      {/each}
     </ul>
   </nav>
 </section>
