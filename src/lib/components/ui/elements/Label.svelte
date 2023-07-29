06971f7a0b2ddeb7c5f21ed842e333f5e0ea10d4
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-07-29 09:10:55 +0200

    apply class attribute if available

--- src/lib/components/ui/elements/Label.svelte
+++ src/lib/components/ui/elements/Label.svelte
@@ -2,7 +2,7 @@
   export let text: string;
 </script>
 
-<label class="flex flex-col gap-1">
+<label class="flex flex-col gap-1 {$$props.class ?? ''}">
   <span class="ml-3">{text}</span>
   <slot />
 </label>
--- src/lib/components/ui/elements/PageCard.svelte
+++ src/lib/components/ui/elements/PageCard.svelte
@@ -1,3 +1,3 @@
-<div class="card p-4 w-full">
+<div class="card p-4 w-full {$$props.class ?? ''}">
   <slot />
 </div>
