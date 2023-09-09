9a0889552399de919cf1e167ac188549959005d1
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-09-09 11:59:48 +0200

    add docker build

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM oven/bun AS build-stage
+
+WORKDIR /app/
+
+COPY package.json yarn.lock .npmrc /app/
+
+RUN bun install
+
+COPY postcss.config.js svelte.config.js tailwind.config.ts theme.ts tsconfig.json vite.config.ts /app/
+COPY src/ /app/src/
+COPY static/ /app/static/
+
+ENV USE_ADAPTER_BUN=true
+
+RUN bunx svelte-kit sync
+RUN bun run build
+
+
+FROM oven/bun AS deploy-stage
+
+WORKDIR /app/
+
+COPY --from=build-stage /app/build/ /app/
+
+EXPOSE 3000
+
+CMD ["bun", "index.js"]
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterBun from 'svelte-adapter-bun';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { resolve } from 'path';
+
+const useAdapterBun = process.env['USE_ADAPTER_BUN'] === 'true';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +15,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: getAdapter(),
     alias: {
       $assets: resolve('./src/assets'),
       $lib: resolve('./src/lib'),
@@ -21,3 +24,11 @@ const config = {
 };
 
 export default config;
+
+function getAdapter() {
+  if (useAdapterBun) {
+    return adapterBun();
+  }
+
+  return adapterAuto();
+}
