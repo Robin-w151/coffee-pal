0970cc88a0d009161c33f4a38c6867c0df6e1c4c
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-09-13 15:23:54 +0200

    show short commit hash

--- src/lib/components/settings/Info.svelte
+++ src/lib/components/settings/Info.svelte
@@ -2,6 +2,7 @@
   import { installEventStore } from '$lib/stores/app';
 
   const appVersion = import.meta.env.APP_VERSION;
+  const appCommitHash = import.meta.env.APP_COMMIT_HASH;
   const itemClass = 'grid grid-cols-[subgrid] col-span-2';
 
   function handleInstallClick(): void {
@@ -12,10 +13,14 @@
 
 <h3 class="h3">Info</h3>
 <div class="flex justify-between items-end">
-  <ul class="grid grid-cols-[min-content_min-content] gap-x-8 gap-y-2">
+  <ul class="grid grid-cols-[max-content_max-content] gap-x-8 gap-y-2">
     <li class={itemClass}>
       <span>Version</span>
-      <strong>{appVersion}</strong>
+      <a
+        href="https://github.com/Robin-w151/coffee-pal/commit/{appCommitHash}"
+        target="_blank"
+        rel="noopener">{appVersion} ({appCommitHash})</a
+      >
     </li>
     <li class={itemClass}>
       <span>Repository</span>
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import manifest from './src/assets/manifest';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+import { execSync } from 'child_process';
 
 export default defineConfig({
   define: {
     'import.meta.env.APP_VERSION': JSON.stringify(process.env.npm_package_version),
+    'import.meta.env.APP_COMMIT_HASH': JSON.stringify(getCommitHash()),
     'process.env.NODE_ENV': '"production"',
   },
   plugins: [
@@ -28,3 +30,7 @@ export default defineConfig({
     host: '0.0.0.0',
   },
 });
+
+function getCommitHash(): string {
+  return execSync('git rev-parse --short=10 HEAD').toString().trim();
+}
