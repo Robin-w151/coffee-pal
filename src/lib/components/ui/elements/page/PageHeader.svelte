3d8a7395ac4778cd82f8524e5213ae64ee7fcb09
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-09-25 14:51:32 +0200

    fix page header layout

--- src/lib/components/ui/elements/page/PageHeader.svelte
+++ src/lib/components/ui/elements/page/PageHeader.svelte
@@ -2,6 +2,6 @@
   export let title: string;
 </script>
 
-<header class="flex items-center w-full h-12">
+<header class="flex items-center px-2 w-full h-12">
   <h2 class="h2">{title}</h2>
 </header>
