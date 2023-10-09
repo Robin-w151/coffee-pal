fbb886d076702d3528c916f9abb483037f42e5ce
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-10-09 20:11:32 +0200

    add windowControlsOverlay change listener

--- src/lib/components/ui/app/AppDragHandle.svelte
+++ src/lib/components/ui/app/AppDragHandle.svelte
@@ -1,7 +1,34 @@
-<div class="drag-handle flex-1 bg-surface-100-800-token" />
+<script lang="ts">
+  import { browser } from '$app/environment';
+  import { debounceTime, fromEvent, tap } from 'rxjs';
+  import type { HasEventTargetAddRemove } from 'rxjs/internal/observable/fromEvent';
+  import { onMount } from 'svelte';
+
+  let visible = browser ? navigator.windowControlsOverlay?.visible ?? false : false;
+
+  onMount(() => {
+    if ('windowControlsOverlay' in navigator) {
+      fromEvent(
+        navigator.windowControlsOverlay as HasEventTargetAddRemove<EventTarget>,
+        'geometrychange',
+      )
+        .pipe(
+          debounceTime(250),
+          tap((change) => {
+            if ('visible' in change) {
+              visible = change.visible ?? 'false';
+            }
+          }),
+        )
+        .subscribe();
+    }
+  });
+</script>
+
+<div class="drag-handle flex-1 bg-surface-100-800-token" class:visible />
 
 <style lang="scss">
-  .drag-handle {
+  .drag-handle.visible {
     -webkit-app-region: drag;
   }
 </style>
