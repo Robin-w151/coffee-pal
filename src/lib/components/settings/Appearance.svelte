c246c13646a1570fd8495065ef7199e2abbb7cc7
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-11-24 15:34:12 +0100

    adjust radio group width

--- src/lib/components/settings/Appearance.svelte
+++ src/lib/components/settings/Appearance.svelte
@@ -2,6 +2,7 @@
   import type { ColorScheme } from '$lib/models/settings';
   import { settingsStore } from '$lib/stores/settings';
   import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+  import Label from '../ui/elements/form/Label.svelte';
 
   let colorScheme: ColorScheme = $settingsStore.colorScheme;
 
@@ -14,10 +15,11 @@
 
 <h3 class="h3">Appearance</h3>
 <div class="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-1">
-  <span>Color Scheme</span>
-  <RadioGroup>
-    <RadioItem name="radio-app-theme" value="system" bind:group={colorScheme}>System</RadioItem>
-    <RadioItem name="radio-app-theme" value="light" bind:group={colorScheme}>Light</RadioItem>
-    <RadioItem name="radio-app-theme" value="dark" bind:group={colorScheme}>Dark</RadioItem>
-  </RadioGroup>
+  <Label text="Color Scheme">
+    <RadioGroup class="radio-group-token">
+      <RadioItem name="radio-app-theme" value="system" bind:group={colorScheme}>System</RadioItem>
+      <RadioItem name="radio-app-theme" value="light" bind:group={colorScheme}>Light</RadioItem>
+      <RadioItem name="radio-app-theme" value="dark" bind:group={colorScheme}>Dark</RadioItem>
+    </RadioGroup>
+  </Label>
 </div>
--- src/lib/components/settings/Units.svelte
+++ src/lib/components/settings/Units.svelte
@@ -2,6 +2,7 @@
   import type { MeasurementSystem } from '$lib/models/measurement';
   import { settingsStore } from '$lib/stores/settings';
   import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+  import Label from '../ui/elements/form/Label.svelte';
 
   let preferredUnits: MeasurementSystem = $settingsStore.preferredUnits;
 
@@ -10,13 +11,14 @@
 
 <h3 class="h3">Units</h3>
 <div class="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-1">
-  <span>Preferred units</span>
-  <RadioGroup>
-    <RadioItem name="radio-preferred-units" value={'metric'} bind:group={preferredUnits}
-      >Metric</RadioItem
-    >
-    <RadioItem name="radio-preferred-units" value={'imperial'} bind:group={preferredUnits}
-      >Imperial</RadioItem
-    >
-  </RadioGroup>
+  <Label text="Preferred units">
+    <RadioGroup class="radio-group-token">
+      <RadioItem name="radio-preferred-units" value={'metric'} bind:group={preferredUnits}
+        >Metric</RadioItem
+      >
+      <RadioItem name="radio-preferred-units" value={'imperial'} bind:group={preferredUnits}
+        >Imperial</RadioItem
+      >
+    </RadioGroup>
+  </Label>
 </div>
