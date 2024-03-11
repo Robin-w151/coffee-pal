4abc39d399814dd950d713e0215c6ad1e8cc53ee
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2024-03-11 10:42:42 +0100

    fix sync availability store bug

--- src/lib/stores/syncAvailability.ts
+++ src/lib/stores/syncAvailability.ts
@@ -3,11 +3,13 @@ import { onlineStore } from 'svelte-legos';
 import { derived, type Readable } from 'svelte/store';
 import { syncStore, type SyncStore } from './sync';
 
+const initialState: SyncAvailability = { isAvailable: false };
 const { subscribe } = derived<[SyncStore, Readable<boolean>], SyncAvailability>(
   [syncStore, onlineStore()],
   ([sync, isOnline], set) => {
-    set({ isAvailable: !!sync && isOnline });
+    set({ isAvailable: !!sync.connection && isOnline });
   },
+  initialState,
 );
 
 export const syncAvailabilityStore = { subscribe };
