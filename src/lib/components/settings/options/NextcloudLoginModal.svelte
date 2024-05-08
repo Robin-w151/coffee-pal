97f2a846b1b62b0d15a734aeb8d513180cc05c93
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2024-05-08 08:06:30 +0200

    fix: adjust nextcloud login modal layout

--- src/lib/components/settings/options/NextcloudLoginModal.svelte
+++ src/lib/components/settings/options/NextcloudLoginModal.svelte
@@ -19,7 +19,7 @@
 <div class="card flex flex-col gap-4 w-full max-w-screen-sm p-4">
   <header class="text-2xl font-bold">Finish Setup</header>
   <article>Please press Login to finish Nextcloud synchronization setup.</article>
-  <div class="flex justify-end gap-4">
+  <footer class="flex justify-end gap-2">
     <button class="btn variant-ghost-primary" on:click={handleCancelClick}>Cancel</button>
     <a
       class="btn variant-filled-primary"
@@ -28,5 +28,5 @@
       rel="noopener"
       on:click={handleLoginClick}>Login</a
     >
-  </div>
+  </footer>
 </div>
