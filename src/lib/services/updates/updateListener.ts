349887e1fe16f220940a7536a2d9cfddde069d5b
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2024-05-09 19:15:06 +0200

    fix(app): check if serviceWorker is available in update listener

--- src/lib/services/updates/updateListener.ts
+++ src/lib/services/updates/updateListener.ts
@@ -5,41 +5,41 @@ export interface UpdateActions {
 let isRefreshing = false;
 
 export async function listenForUpdates(callback: (actions: UpdateActions) => void) {
-  navigator.serviceWorker.addEventListener('controllerchange', () => {
-    if (isRefreshing) {
+  if ('serviceWorker' in navigator && navigator.serviceWorker) {
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      if (isRefreshing) {
+        return;
+      }
+
+      isRefreshing = true;
+      window.location.reload();
+    });
+
+    const registration = await navigator.serviceWorker.ready;
+    const restart = () => {
+      registration.waiting?.postMessage('SKIP_WAITING');
+    };
+    const actions: UpdateActions = { restart };
+    const awaitStateChange = () => {
+      registration.installing?.addEventListener('statechange', function () {
+        if (this.state === 'installed') {
+          callback(actions);
+        }
+      });
+    };
+
+    if (!registration) {
       return;
     }
 
-    isRefreshing = true;
-    window.location.reload();
-  });
+    if (registration.waiting) {
+      return callback(actions);
+    }
 
-  const registration = await navigator.serviceWorker.ready;
-  const actions: UpdateActions = { restart };
+    if (registration.installing) {
+      awaitStateChange();
+    }
 
-  function restart() {
-    registration.waiting?.postMessage('SKIP_WAITING');
+    registration.addEventListener('updatefound', awaitStateChange);
   }
-
-  function awaitStateChange() {
-    registration.installing?.addEventListener('statechange', function () {
-      if (this.state === 'installed') {
-        callback(actions);
-      }
-    });
-  }
-
-  if (!registration) {
-    return;
-  }
-
-  if (registration.waiting) {
-    return callback(actions);
-  }
-
-  if (registration.installing) {
-    awaitStateChange();
-  }
-
-  registration.addEventListener('updatefound', awaitStateChange);
 }
