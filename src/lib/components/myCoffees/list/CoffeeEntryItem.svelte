0b4f9578befc2e8384549ac21896ab5e5738b0c2
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2024-07-25 23:00:20 +0200

    fix: remove unnecessary div

--- src/lib/components/myCoffees/list/CoffeeEntryItem.svelte
+++ src/lib/components/myCoffees/list/CoffeeEntryItem.svelte
@@ -13,17 +13,15 @@
   }
 </script>
 
-<div class="p-0">
-  <a
-    class="block min-w-0 flex-1 px-3 py-2 hover:bg-primary-500/10 rounded-md"
-    href="/my-coffees/{entry.id}"
-  >
-    <dt class="overflow-hidden text-ellipsis whitespace-nowrap">
-      <span>{title(entry)}</span>
-    </dt>
-    <dd class="font-normal overflow-hidden text-ellipsis whitespace-nowrap">
-      <span>{details(entry)}</span>
-    </dd>
-  </a>
-</div>
+<a
+  class="block min-w-0 flex-1 px-3 py-2 hover:bg-primary-500/10 rounded-md"
+  href="/my-coffees/{entry.id}"
+>
+  <dt class="overflow-hidden text-ellipsis whitespace-nowrap">
+    <span>{title(entry)}</span>
+  </dt>
+  <dd class="font-normal overflow-hidden text-ellipsis whitespace-nowrap">
+    <span>{details(entry)}</span>
+  </dd>
+</a>
 <hr />
